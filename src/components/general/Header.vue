0ec51ad3378796550aca071e547f0ef44b074bea
unknown <alexlopez201510@gmail.com>
2025-09-08 18:54:32 +0200

    Update: Updated header

--- src/components/general/Header.vue
+++ src/components/general/Header.vue
@@ -1,8 +1,11 @@
 <template>
-    <div>
-        header
+    <div class="mainHeader">
+        <img class="logo" src="../../assets/imgs/logo.svg" alt="logo">
+        <nav class="navBar">
+            <router-link class="title weight-700 link" :to="route.path" v-for="route in routes" :key="route">{{ route.name }}</router-link>
+        </nav>
     </div>
 </template>
 <script setup>
-
+import routes from '../../router/routes'
 </script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,9 +2,30 @@ import Home from "@/pages/Home.vue"
 const routes = [
     {
         path:'/',
-        name:'Home',
+        name:'HOME',
         component: Home
-    }
+    },
+    {
+        path:'/about',
+        name:'ABOUT',
+        component: Home
+    },
+    {
+        path:'/projects',
+        name:'PROJECTS',
+        component: Home
+    },
+    {
+        path:'/skills',
+        name:'SKILLS',
+        component: Home
+    },
+
+    {
+        path:'/expecierce',
+        name:'EXPERIENCE',
+        component: Home
+    },
 ]
 
 export default routes
