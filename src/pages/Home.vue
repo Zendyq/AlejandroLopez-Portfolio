89a3ebec382b3942bee250b7e83307eded3423f2
unknown <alexlopez201510@gmail.com>
2025-09-16 20:30:53 +0200

    Add: Added colorful shapes

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,21 +1,23 @@
 <template>
-    <div class="mainContainer noselect">
-        <div class="homeSchedule">
-            <div>
-                <div></div>
-                <div></div>
-                <img src="@/assets/imgs/FrontSidePicture.svg" alt="Picture of the client">
-            </div>
-            <div>
-                <h1 class="titleHome title">Alejandro López Gasco Fernández</h1>
-                <p class="subtitleHome title">Full-stack developer</p>
-                <div class="skillsHome">
-                    <img src="" alt="Tecnologías">
-                </div>
-            </div>
+  <div class="mainContainer noselect">
+    <div class="homeSchedule">
+      <div class="profileContainer">
+        <div class="profileImgWrapper">
+          <img class="profileImg" src="@/assets/imgs/FrontSidePicture.svg" alt="Picture of the client">
         </div>
+      </div>
+
+      <div>
+        <h1 class="titleHome title">Alejandro López Gasco Fernández</h1>
+        <p class="subtitleHome title">Full-stack developer</p>
+        <div class="skillsHome">
+          <img src="" alt="Tecnologías">
+        </div>
+      </div>
     </div>
+  </div>
 </template>
+
 <script setup>
 
 </script>
